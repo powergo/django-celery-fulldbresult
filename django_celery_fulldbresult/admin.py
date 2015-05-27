5a4dff9bfc5ba9709f61c51f1c3b45eeb170ba19
Barthelemy Dagenais <bart@resulto.ca>
2015-05-27 10:58:31 -0400

    Added default adminfor TaskResultMeta. refs #1

--- django_celery_fulldbresult/admin.py
+++ django_celery_fulldbresult/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from django_celery_fulldbresult.models import TaskResultMeta
+
+
+class TaskResultMetaAdmin(admin.ModelAdmin):
+    list_display = ("task_id", "task", "date_done", "status")
+    search_fields = ("task_id", "task")
+    list_filter = ("status", )
+
+
 # Register your models here.
+admin.site.register(TaskResultMeta, TaskResultMetaAdmin)
