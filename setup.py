d5d9f1661e16007c9d90ebe14390a0876aee998c
Alexandre Arpin <arpin.alexandre@gmail.com>
2015-07-22 14:14:46 -0400

    cleanup. Fixed Github URL in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/result-admin/django-celery-fulldbresult',
+    url='https://github.com/resulto-admin/django-celery-fulldbresult',
 
     # Author details
     author='Resulto Dev Team',
