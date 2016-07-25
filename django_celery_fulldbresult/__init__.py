4fcb0d1ecf93267ff9bdc6776ce89e6aa6b823e6
Barthelemy Dagenais <bart@resulto.ca>
2016-07-25 15:49:19 -0400

    Stop publish if eta is detected and save task as scheduled. #refs 15

--- django_celery_fulldbresult/__init__.py
+++ django_celery_fulldbresult/__init__.py
@@ -1,16 +1,37 @@
 from celery import current_app
 from celery.states import PENDING
-from celery.app.task import Context
+from celery.app.task import Context, Task
 from celery.signals import before_task_publish
+from django_celery_fulldbresult.errors import SchedulingStopPublishing
 
 from django.conf import settings
 from django.utils.timezone import now
 
 
+old_apply_async = Task.apply_async
+
+
+def new_apply_async(
+        self, *args, **kwargs):
+    try:
+        return old_apply_async(self, *args, **kwargs)
+    except SchedulingStopPublishing as exc:
+        #
+        return self.AsyncResult(exc.task_id)
+
+
+Task.apply_async = new_apply_async
+
+
+# TODO MAY NEED ANOTHER OPTION AND ANOTHER RECEIVER IN CASE SOMEONE WANTS TO
+# ABOLISH ETA BUT NOT TRACK PUBLISH?
 if getattr(settings, "DJANGO_CELERY_FULLDBRESULT_TRACK_PUBLISH", False):
     @before_task_publish.connect
     def update_sent_state(sender=None, body=None, exchange=None,
                           routing_key=None, **kwargs):
+        # App may not be loaded on init
+        from django_celery_fulldbresult.models import SCHEDULED
+
         if not getattr(
                 settings, "DJANGO_CELERY_FULLDBRESULT_TRACK_PUBLISH", False):
             # Check again to support dynamic change of this settings
@@ -31,5 +52,16 @@ if getattr(settings, "DJANGO_CELERY_FULLDBRESULT_TRACK_PUBLISH", False):
             "exchange": exchange,
             "routing_key": routing_key
         }
-        backend.store_result(body["id"], None, PENDING, traceback=None,
+
+        # TODO More complex check: no chord, no group, etc.
+        # print(body)
+        if body.get("eta"):
+            status = SCHEDULED
+        else:
+            status = PENDING
+
+        backend.store_result(body["id"], None, status, traceback=None,
                              request=request)
+
+        if status == SCHEDULED:
+            raise SchedulingStopPublishing(task_id=body["id"])
---
+++ django_celery_fulldbresult/errors.py
@@ -0,0 +1,8 @@
+
+class SchedulingStopPublishing(Exception):
+    """Raised before publishing a scheduled task to prevent Celery from sending
+    the task to the broker.
+    """
+    def __init__(self, task_id):
+        super(SchedulingStopPublishing, self).__init__()
+        self.task_id = task_id
